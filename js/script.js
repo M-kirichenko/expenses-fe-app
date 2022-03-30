243004274441cad61aef4a278a017498b828d0b3
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-03-30 17:42:09 +0400

    add function

--- js/script.js
+++ js/script.js
@@ -5,9 +5,39 @@ class Expenses {
   expName = document.querySelector("#exp-name");
 
   add() {
-    console.log("test add");
+    const priceVal = this.expPrice.value;
+    const nameVal = this.expName.value;
+  
+    if(!priceVal.length || !nameVal.length) {
+      this.expPrice.classList.add("warn-border");
+      this.expName.classList.add("warn-border");
+    } else {
+      const prevData = this.getData();
+      const obj = {name: nameVal, price: priceVal};
+  
+      if(!prevData) {
+        const arr = [];
+        arr.push(obj);
+        this.setData(arr);
+      } else {
+        prevData.push(obj);
+        this.setData(prevData);
+      }
+    }
+    this.expName.value = "";
+    this.expPrice.value = "";
   }
-
+  
+  getData() {
+    if(localStorage.expenses) return JSON.parse(localStorage.expenses);
+    return false;
+  }
+  
+  setData(data) {
+    const dataToString = JSON.stringify(data);
+    localStorage.expenses = dataToString;
+  }
+  
   use() {
     this.addButton.onclick = () => this.add();
   }
